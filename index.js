df0cdc6b77dd52533244c1f38ac3fb30720c67f1
gsandss <gavinsands11@gmail.com>
2026-03-31 18:13:06 -0500

    home page visuals

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("mousemove", (e) =>{
+    const x = (e.clientX / window.innerWidth - 0.5) *20;
+    const y = (e.clientY / window.innerHeight - 0.5) *20;
+    
+    document.body.style.backgroundPosition = `${50 + x}% ${50 + y}%`;
+});
